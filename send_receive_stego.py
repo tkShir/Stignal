9378ec90cea9972297e440befd0cba01d3061fe0
Taka Shirono <takashirono@gmail.com>
2020-04-26 17:07:51 -0400

    Initialized necessart components

---
+++ send_receive_stego.py
@@ -0,0 +1,6 @@
+"""
+2. Send stego file
+input: stego file, ip address
+3. Receive stego file
+output: stego file, sender's ip
+"""
---
+++ stego_to_text.py
@@ -0,0 +1,5 @@
+"""
+4. Convert stego file back to text
+input: stego file,key
+output: text file
+"""
