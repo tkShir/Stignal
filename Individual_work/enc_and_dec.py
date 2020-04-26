56dacc9f400dec12a7d419d7b333ee342e866f4a
Taka Shirono <takashirono@gmail.com>
2020-04-26 17:10:38 -0400

    Started working on encryption and decryption module

---
+++ Individual_work/enc_and_dec.py
@@ -0,0 +1,7 @@
+"""
+for 4 and 5. Our choice for the encryption scheme is AES-CTR, python implementation is found here:
+    https://pycryptodome.readthedocs.io/en/latest/src/cipher/aes.html
+We will apply AES-CTR on the message before it is transformed.
+So we need to encrypt the message and store it.
+This library is trusted.
+"""
