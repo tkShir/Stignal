f7c92d933dd686c6da3751666243c824d14ca7bf
Michael Korovkin <mckoro@bu.edu>
2020-04-28 15:23:06 -0700

    comis

--- Individual_work/packet_receiver_server.py
+++ Individual_work/packet_receiver_server.py
@@ -1,6 +1,7 @@
 import sys
 import socket
 import select
+from enc_and_dec import decrypt
 
 HOST = '' 
 SOCKET_LIST = []
@@ -8,7 +9,6 @@ RECV_BUFFER = 4096
 PORT = 9009
 
 def chat_server():
-
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     server_socket.bind((HOST, PORT))
@@ -17,7 +17,7 @@ def chat_server():
     # add server socket object to the list of readable connections
     SOCKET_LIST.append(server_socket)
  
-    print "Chat server started on port " + str(PORT)
+    print("Chat server started on port " + str(PORT))
  
     while 1:
 
@@ -30,7 +30,7 @@ def chat_server():
             if sock == server_socket: 
                 sockfd, addr = server_socket.accept()
                 SOCKET_LIST.append(sockfd)
-                print "Client (%s, %s) connected" % addr
+                print("Client (%s, %s) connected" % addr)
                  
                 broadcast(server_socket, sockfd, "[%s:%s] entered our chatting room\n" % addr)
              
@@ -42,8 +42,8 @@ def chat_server():
                     data = sock.recv(RECV_BUFFER)
                     if data:
                         # there is something in the socket
-                        do_something_with_incoming_data(data)
-                        broadcast(server_socket, sock, "\r" + '[' + str(sock.getpeername()) + '] ' + data)  
+                        output_data = do_something_with_incoming_data(data)
+                        broadcast(server_socket, sock, "\r" + '[' + str(sock.getpeername()) + '] ' + output_data)  
                     else:
                         # remove the socket that's broken    
                         if sock in SOCKET_LIST:
@@ -60,10 +60,12 @@ def chat_server():
     server_socket.close()
 
 def do_something_with_incoming_data(data):
-	pass
+	zi = data.split("|||")
+	decrypted_data = decrypt()
+	return decrypted_data
     
 # broadcast chat messages to all connected clients
-def broadcast (server_socket, sock, message):
+def broadcast(server_socket, sock, message):
     for socket in SOCKET_LIST:
         # send the message only to peer
         if socket != server_socket and socket != sock :
