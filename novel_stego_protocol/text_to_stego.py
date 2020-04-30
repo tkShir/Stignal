4f32552faf8fe63ce4d5f3d456f09ae8242f5a29
Taka Shirono <takashirono@gmail.com>
2020-04-30 15:05:48 -0400

    text to stego addition

--- novel_stego_protocol/text_to_stego.py
+++ novel_stego_protocol/text_to_stego.py
@@ -1,5 +1,10 @@
-"""
-1. Convert text to stego file
-input: text file,key
-output: stego file
-"""
+from textgenrnn import textgenrnn
+
+textgen = textgenrnn('/home/amazonec2/hacker_news.hdf5')
+
+def text_to_stego(ciphertext_to_steg):
+    stegotext = textgen.generate(interactive=True, temperature=0.2, top_n=2, ciphertext=ciphertext_to_steg)
+    print(stegotext)
+    return stegotext
+
+text_to_stego(b'rnaodmdomeodshit')
